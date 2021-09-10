b90fbb2030251f74845b1f44ceb4d1deaa4cafd5
Toby Dylan Hocking <toby.hocking@nau.edu>
2021-09-10 08:51:12 -0700

    all timings microbenchmark demo

---
+++ demos/2021-09-10.R
@@ -0,0 +1,86 @@
+head(iris)
+data.mat <- as.matrix(
+  iris[, c("Petal.Width", "Petal.Length")])
+head(data.mat)
+str(data.mat)
+library(ggplot2)
+ggplot()+
+  geom_point(aes(
+    x=Petal.Length,
+    color=Species,
+    y=Petal.Width),
+    data=iris)
+
+library(mclust)
+n.clusters <- 3
+system.time({
+  mclust.result <- mclust::Mclust(
+    data.mat, n.clusters, "VVI", verbose=FALSE)
+})
+mclust.result$loglik
+mclust.result$classification
+?mclust::Mclust
+mclust.options("subset")
+
+big.data.mat <- data.mat[rep(1:nrow(data.mat), 10),]
+str(big.data.mat)
+system.time({
+  mclust.result <- mclust::Mclust(
+    big.data.mat, n.clusters, "VVI", verbose=FALSE)
+})
+
+system.time({
+  kmeans(data.mat, n.clusters)
+})
+system.time({
+  kmeans(big.data.mat, n.clusters)
+})
+
+system.time({
+  mclust.result <- mclust::Mclust(
+    big.data.mat, n.clusters, "VVI", verbose=FALSE,
+    initialization = list(
+      subset=1:150))
+})
+
+library(data.table)
+(n.data.vec <- as.integer(10^seq(1, 3, by=0.5)))
+all.timings.list <- list()
+for(n.data in n.data.vec){
+  n.data.mat <- big.data.mat[1:n.data,]
+  n.timings <- microbenchmark::microbenchmark(
+    mclust.init={
+      mclust.result <- mclust::Mclust(
+        n.data.mat, n.clusters, "VVI", verbose=FALSE,
+        initialization = list(
+          subset=1:min(150, nrow(n.data.mat))))
+    },
+    mclust.default={
+      mclust.result <- mclust::Mclust(
+        n.data.mat, n.clusters, "VVI", verbose=FALSE)
+    },
+    kmeans={
+      kmeans(n.data.mat, n.clusters)
+    },
+    times=10)
+  class(n.timings)
+  all.timings.list[[paste(n.data)]] <- data.table(
+    n.data,
+    n.timings)
+}
+(all.timings <- do.call(rbind, all.timings.list))
+all.timings[, seconds := time/1e9]
+all.timings[, algorithm := expr]
+
+library(ggplot2)
+ggplot()+
+  geom_point(aes(
+    n.data, seconds, color=algorithm),
+    data=all.timings)
+
+ggplot()+
+  geom_point(aes(
+    n.data, seconds, color=algorithm),
+    data=all.timings)+
+  scale_x_log10()+
+  scale_y_log10()

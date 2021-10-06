521cc2850747e63d051e20a5dc9603ecf57e0d7a
Toby Dylan Hocking <toby.hocking@nau.edu>
2021-10-06 08:48:58 -0700

    nb demo

---
+++ demos/2021-10-06.R
@@ -0,0 +1,11 @@
+data(neuroblastoma, package="neuroblastoma")
+library(data.table)
+ls()
+(nb.dt <- data.table(neuroblastoma$profiles))
+(data.dt <- nb.dt[profile.id=="1" & chromosome=="1"])
+
+library(ggplot2)
+ggplot()+
+  geom_point(aes(
+    position, logratio),
+    data=data.dt)
